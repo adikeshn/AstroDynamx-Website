3ad966bb5710454c4b955062a9e3a9877bd7bf3b
Turtlez200 <88110937+adikeshn@users.noreply.github.com>
2023-12-12 20:14:18 -0500

    First Commit Started Landing Page

---
+++ astrodynamx-website/src/LandingPage.jsx
@@ -0,0 +1,13 @@
+import "./index.css";
+import TopBar from "./components/TopBar";
+
+function LandingPage() {
+  return (
+    <div className="screen">
+      <TopBar />
+      <div className="backgroundDiv"></div>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ astrodynamx-website/src/components/TopBar.jsx
@@ -0,0 +1,11 @@
+import "../index.css";
+
+function TopBar() {
+  return (
+    <div className="topBar">
+      <img className="topbarLogo" src="src/assets/LogoSide.png" />
+    </div>
+  );
+}
+
+export default TopBar;
