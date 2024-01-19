4c3262f2faacaf7cdda35fdba9843082ace4509b
LIGHTNINGSUN144 <97405224+LIGHTNINGSUN144@users.noreply.github.com>
2024-01-19 16:39:09 -0500

    Adds a button to the home page that links to a google form to add classes to the user's account.

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import FirebaseInfo from "../../firebase-config";
 import { useNavigate } from "react-router-dom";
+import "./HomePage.css";
+
+
 function HomePage() {
   const navigate = useNavigate();
 
@@ -9,7 +12,23 @@ function HomePage() {
     navigate("/");
   };
 
-  return <button onClick={signout}>hello world</button>;
-}
+  return (
+    <div className="outside-class">
+      <div className="add-class-box">
+        <a href="https://docs.google.com/forms/d/e/1FAIpQLSeer3eGO5dAdOpEoN02hY01ZdAjjIjr6XrIWtWIx4MtrV4k6g/viewform?usp=sf_link" target="_blank">
+          <div className="add-class">
+            Join a class
+          </div>
+          <img src="AddClassIcon.png" className="class-img" />
+        </a>
+      </div>
+      <div className="logout-button">
+        <button onClick={signout}>Log out</button>
+      </div>
+    </div>
+  );
 
-export default HomePage;
+}
+  
+  export default HomePage;
+//<button onClick={signout}>hello world</button>;
