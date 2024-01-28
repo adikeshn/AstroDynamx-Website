ecf48d6b394418f3cfa745259fbd285fca323b30
arnav <arnav.bholausa@gmail.com>
2024-01-28 10:38:24 -0500

    adding dynamic styles

--- src/components/Landing/BottomBar/BottomBar.jsx
+++ src/components/Landing/BottomBar/BottomBar.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-import "./BottomBar.scss";
+import { useState, useEffect, useCallback } from "react";
 import { RiInstagramLine, RiLinkedinBoxLine } from "react-icons/ri";
+
+import styles from "./BottomBar.module.scss";
 
 function BottomBar() {
   //This is for the get involved popup
@@ -8,7 +9,8 @@ function BottomBar() {
   const handleShowPopup = () => {
     setShowPopup(true);
   };
-  const handleClosePopup = (event) => {
+
+  const handleClosePopup = useCallback((event) => {
     // Close the popup only if the click is outside the popup or the show button
     if (
       (!event.target.closest(".popup") && !event.target.closest("#popup-button")) ||
@@ -16,12 +18,17 @@ function BottomBar() {
     ) {
       setShowPopup(false);
     }
-  };
-  function handleKeyDown(event) {
-    if (event.key === "Escape" && showPopup) {
-      setShowPopup(false);
-    }
-  }
+  }, []);
+
+  const handleKeyDown = useCallback(
+    (event) => {
+      if (event.key === "Escape" && showPopup) {
+        setShowPopup(false);
+      }
+    },
+    [showPopup]
+  );
+
   useEffect(() => {
     // Add event listener to close the popup on outside clicks
     document.addEventListener("click", handleClosePopup);
@@ -32,26 +39,26 @@ function BottomBar() {
       document.removeEventListener("click", handleClosePopup);
       document.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [handleKeyDown, handleClosePopup]);
 
   return (
-    <div className='bottom-div'>
-      <div className='get-involved'>
+    <div className={styles["bottom-div"]}>
+      <div className={styles["get-involved"]}>
         <button
-          id='popup-button'
+          className={styles["popup-button"]}
           onClick={handleShowPopup}
         >
-          <span className='get-involved-text'>Get Involved</span>
+          <span className={styles["get-involved-text"]}>Get Involved</span>
         </button>
         {showPopup && (
-          <div className='popup'>
+          <div className={styles["popup"]}>
             <h2>Join Us!</h2>
-            <div className='involved-text'>
+            <div className={styles["involved-text"]}>
               Send your resume to
               <a href='mailto:astrodynamx@gmail.com?subject=Applicaton'> astrodynamx@gmail.com!</a>
             </div>
             <button
-              id='close-button'
+              className={styles["close-button"]}
               onClick={handleClosePopup}
             >
               &times;
@@ -59,19 +66,19 @@ function BottomBar() {
           </div>
         )}
       </div>
-      <div className='icon-div'>
+      <div className={styles["icon-div"]}>
         <RiInstagramLine
           size={46}
           color='white'
           onClick={() => {
             window.open("https://www.instagram.com/astrodynamx/", "_blank");
           }}
-          className='icon-cursor'
+          className={styles["icon-cursor"]}
         />
         <RiLinkedinBoxLine
           size={46}
           color='white'
-          className='icon-cursor'
+          className={styles["icon-cursor"]}
           onClick={() => {
             window.open(
               "https://www.linkedin.com/company/astrodynamx/about/?viewAsMember=true",
@@ -80,7 +87,7 @@ function BottomBar() {
           }}
         />
       </div>
-      <div className='space-corrcter'></div>
+      <div className={styles["space-corrcter"]}></div>
     </div>
   );
 }
