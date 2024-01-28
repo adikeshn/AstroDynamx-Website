05ad0537b894882f9c7ca3d9579cf46a3f24484a
arnav <arnav.bholausa@gmail.com>
2024-01-27 22:42:35 -0500

    updating homepage

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import router from "./pages/router";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/pages/routes/404/404.jsx
@@ -0,0 +1,7 @@
+import styles from "./404.module.scss";
+
+const ErrorPage = () => {
+  return <div className={styles["container"]}></div>;
+};
+
+export default ErrorPage;
